c4364efbaab22675297563971a549e9bf8bda5c9
Shalini Nayan <shalininayan@outlook.com>
2024-03-14 17:25:22 +0000

    Learn use of console.tabble

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,3 +2,13 @@ const accountId = 144553
 let accountEmail = "shalini@binni.com"
 var accountPassword = "12345"
 accountCity = "Delhi"
+
+// accountId = 2 // not allowed
+
+accountEmail = "nayan@shinine.com"
+accountPassword = "11112222"
+accountCity = "Indore"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
