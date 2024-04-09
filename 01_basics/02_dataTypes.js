e6097141a804931a98321154fd8bcb246d4163b2
Shalini Nayan <shalininayan@outlook.com>
2024-04-09 17:56:15 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3) // code readability should be high 
+
+console.log("Shalini")
+
+let name = "shalini" //string
+let age = 18 //number
+let isLoggedIn = false //boolean
+
+
+// number => 2 to the power 53(is range)
+// bigint ---> generally use for big numbers like share market and all
+// string => ""
+// boolean => true/false
+// null => standalone value(representative of empty)
+// undefined => value not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
