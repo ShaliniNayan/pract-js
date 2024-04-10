5b1560d6ef402174619b8ddec85c5a816ae16c37
Shalini Nayan <shalininayan@outlook.com>
2024-04-10 17:46:12 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// NaN --> Not in number
+// "33" => 33
+// "33abc" => Nan
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "shalini" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
