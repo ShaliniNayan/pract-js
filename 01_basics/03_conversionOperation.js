f2ab873391ab92702d7939a0b6e6dd1530bfdb2e
Shalini Nayan <shalininayan@outlook.com>
2024-04-17 17:02:35 +0000

    Data type operation & conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -44,9 +44,27 @@ let str1 = "hello"
 let str2 = " shalini"
 
 let str3 = str1 +str2
-console.log(str3); // hello shalini
+// console.log(str3); // hello shalini
 
-console.log("1" + 2); // 12
-console.log(1 + "2"); // 12
-console.log("1" + 2 + 2); // 122
-console.log(1 + 2 + "2"); // 32
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log(true); // true
+// console.log(+true); // 1 --> boolean is a also number it add increment 
+// console.log(+""); // 0
+
+let num1, num2, num3
+num1= num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter); // 101
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter); // 101
+
+// Link to --> https://tc39.es/ecma262/multipage/abstract-operations.html#sec-toprimitive
+// Link to --> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
