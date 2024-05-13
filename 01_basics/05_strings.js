ac523991ae174ba06d60e09cb7ea0a63c55a683e
Shalini Nayan <shalininayan@outlook.com>
2024-05-13 09:01:41 +0000

    Update the method of string

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -8,7 +8,7 @@ const repoCount = 90
 console.log(`Hello my name is ${name} amy my repo count is ${repoCount}`);
 
 // Declare string 
-const gameName = new String('shalini-sn')
+const gameName = new String('shalini-sn-com')
 
 console.log(gameName[0]); // key value access 
 console.log(gameName.__proto__); // proto type object access. There are double underscore
@@ -18,8 +18,24 @@ console.log(gameName.toUpperCase()); // Write in capital letter
 console.log(gameName.charAt(2)); // To know the position of character
 console.log(gameName.indexOf('i')); // To know the in which position the character is
 
-const newString = gameName.substring(0, 5)
+const newString = gameName.substring(0, 5) // We can't give negative value in substring. It will ignore it.
 console.log(newString);
 
 const anotherString = gameName.slice(-9, 5) // In slice we can give negative value which give answer in reverse order.
 console.log(anotherString); 
+
+const newStringOne = "   shalini    "
+console.log(newStringOne);
+console.log(newStringOne.trim()); // It remove the space which is no use.
+// link for trim documentation https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/trim
+
+const url = "https://shalini.com/shalini%20nayan"
+
+console.log(url.replace('%20', '-')); //.replace works as find the character and replace with new.
+
+console.log(url.includes('shalini')); // .includes works as searching and giving answer in true or false
+console.log(url.includes('sandra')); // .includes works as searching and giving answer in true or false
+
+console.log(gameName.split('-')); // It create array and bi farcate the charcter in given object.
+// link for split documentation https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+
