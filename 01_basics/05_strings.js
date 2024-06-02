cde16f042391e098d8fd937f85b17f08ee9b2974
Shalini Nayan <shalininayan@outlook.com>
2024-06-02 17:14:07 +0000

    Update console

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -37,6 +37,7 @@ console.log(url.includes('shalini')); // .includes works as searching and giving
 console.log(url.includes('sandra')); // .includes works as searching and giving answer in true or false
 console.log(url.includes('aarav')); 
 console.log(url.includes('aadit'));
+console.log(url.includes('ashita'));
 
 console.log(gameName.split('-')); // It create array and bi farcate the charcter in given object.
 // link for split documentation https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
