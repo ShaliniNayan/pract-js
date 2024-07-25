98b9dc508f8c5ef49de10a6a1a81cdd4aed061e2
Shalini Nayan <shalininayan@outlook.com>
2024-07-25 14:30:30 +0000

    Add new method to date in js

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -18,3 +18,4 @@ let myCreatedDate = new Date("2024-07-23") // To create specific or current date
 // console.log(myCreatedDate.toLocaleString()); // 7/23/2024, 11:50:00 PM
 console.log(myCreatedDate.toLocaleString()); // 7/23/2024, 12:00:00 AM
 console.log(myCreatedDate.toLocaleTimeString()); // 7/23/2024, 12:00:00 AM
+console.log(myCreatedDate.toISOString()); // 7/23/2024, 12:00:00 AM
