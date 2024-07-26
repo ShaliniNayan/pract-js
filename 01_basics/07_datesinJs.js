80e4e15364879e7eda5c00a72c6e514c6c7ba924
Shalini Nayan <shalininayan@outlook.com>
2024-07-26 07:06:27 +0000

    Time zone offset console

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -19,3 +19,4 @@ let myCreatedDate = new Date("2024-07-23") // To create specific or current date
 console.log(myCreatedDate.toLocaleString()); // 7/23/2024, 12:00:00 AM
 console.log(myCreatedDate.toLocaleTimeString()); // 12:00:00 AM --> only time will displayed
 console.log(myCreatedDate.toISOString()); // 2024-07-23T00:00:00.000Z 
+console.log(myCreatedDate.getTimezoneOffset()); // 0  
