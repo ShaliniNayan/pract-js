20b21c753ef93f6e10ba8871be0b277abac9d745
Shalini Nayan <shalininayan@outlook.com>
2024-07-26 07:04:26 +0000

    Update other method with console

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -17,5 +17,5 @@ let myCreatedDate = new Date("2024-07-23") // To create specific or current date
 //console.log(myCreatedDate.toDateString()); // Tue Jan 23 2024
 // console.log(myCreatedDate.toLocaleString()); // 7/23/2024, 11:50:00 PM
 console.log(myCreatedDate.toLocaleString()); // 7/23/2024, 12:00:00 AM
-console.log(myCreatedDate.toLocaleTimeString()); // 7/23/2024, 12:00:00 AM
-console.log(myCreatedDate.toISOString()); // 7/23/2024, 12:00:00 AM
+console.log(myCreatedDate.toLocaleTimeString()); // 12:00:00 AM --> only time will displayed
+console.log(myCreatedDate.toISOString()); // 2024-07-23T00:00:00.000Z 
