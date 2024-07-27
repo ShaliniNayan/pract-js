c036f0fbdcd8aaa5944257d7f726bd8a93ebcf94
Shalini Nayan <shalininayan@outlook.com>
2024-07-27 15:29:41 +0000

    Time setup

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -16,7 +16,9 @@ let myCreatedDate = new Date("2024-07-23") // To create specific or current date
 //console.log(myCreatedDate); // 2024-01-23T00:00:00.000Z
 //console.log(myCreatedDate.toDateString()); // Tue Jan 23 2024
 //console.log(myCreatedDate.toLocaleString()); // 7/23/2024, 11:50:00 PM
-console.log(myCreatedDate.toLocaleString()); // 7/23/2024, 12:00:00 AM
-console.log(myCreatedDate.toLocaleTimeString()); // 12:00:00 AM --> only time will displayed
-console.log(myCreatedDate.toISOString()); // 2024-07-23T00:00:00.000Z 
-console.log(myCreatedDate.getTimezoneOffset()); // 0  
+//console.log(myCreatedDate.toLocaleString()); // 7/23/2024, 12:00:00 AM
+//console.log(myCreatedDate.toLocaleTimeString()); // 12:00:00 AM --> only time will displayed
+//console.log(myCreatedDate.toISOString()); // 2024-07-23T00:00:00.000Z 
+//console.log(myCreatedDate.getTimezoneOffset()); // 0
+
+let myTimeStamp
