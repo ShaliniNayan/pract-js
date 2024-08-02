644ae475b10dae2ec23b0e177611ca46718c9521
Shalini Nayan <shalininayan@outlook.com>
2024-08-02 16:28:05 +0000

    Learn slice & splice difference

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -7,7 +7,7 @@ const myArr = [0, 1, 2, 3, 4, 5]
 const myHeors = ["shaktiman", "nagraj"]
 
 const myArr2 = new Array(1, 2, 3, 4)
-console.log(myArr[1]);
+// console.log(myArr[1]);
 
 // Arrey Methods
 
@@ -24,6 +24,19 @@ console.log(myArr[1]);
 
 const newArr = myArr.join() // 0,1,2,3,4,5 --> Combine the value into sing string
 
-console.log(myArr);
-console.log(newArr);
-console.log(typeof newArr); // string
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr); // string
+
+// slice, splice => splice manuplate the original array so careful to check which one is manuplating the original array
+
+console.log("A", myArr); // A [ 0, 1, 2, 3, 4, 5 ]
+
+const myn1 = myArr.slice(1, 3) 
+
+console.log(myn1); // [ 1, 2 ]
+console.log("B", myArr); // B [ 0, 1, 2, 3, 4, 5 ]
+
+const myn2 = myArr.splice(1, 3)
+console.log("C", myArr); // C [ 0, 4, 5 ]
+console.log(myn2) // [ 1, 2, 3 ]
