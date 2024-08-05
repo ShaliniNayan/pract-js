e6ef9c22f18a890165548ef93394f1b7624a7b99
Shalini Nayan <shalininayan@outlook.com>
2024-08-05 15:56:02 +0000

    concat method

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,10 +1,11 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-marvel_heros.push(dc_heros)
+// marvel_heros.push(dc_heros)
 
 // console.log(marvel_heros); // [ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ] --> array can take any data
 // console.log(marvel_heros[3][1]); // [ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
 // flash
 
-
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros); // [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ] --> merge the exiting array and give one array
