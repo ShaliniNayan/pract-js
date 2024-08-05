e33e34402451792a6b82eed403b1d43c68908f14
Shalini Nayan <shalininayan@outlook.com>
2024-08-05 15:47:02 +0000

    method push

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,1 +1,10 @@
-// array method other than 
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros); // [ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ] --> array can take any data
+// console.log(marvel_heros[3][1]); // [ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+// flash
+
+
