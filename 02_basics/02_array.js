11ccb3230d68cbaf381b4dc59ecf9a05d69d6554
Shalini Nayan <shalininayan@outlook.com>
2024-08-06 15:49:14 +0000

    Spread operator

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -7,5 +7,9 @@ const dc_heros = ["superman", "flash", "batman"]
 // console.log(marvel_heros[3][1]); // [ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
 // flash
 
-const allHeros = marvel_heros.concat(dc_heros)
-console.log(allHeros); // [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ] --> merge the exiting array and give one array
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros); // [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ] --> merge the exiting array and give one array
+
+const all_new_heros = [...marvel_heros, ...dc_heros] // ... --> it's called spread
+
+console.log(all_new_heros); // [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ] --> same as concat
