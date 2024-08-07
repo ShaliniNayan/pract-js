a613c2c1aba541ed2cb767edec542c1fd2345d79
Shalini Nayan <shalininayan@outlook.com>
2024-08-07 17:23:00 +0000

    Learn object literals

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,3 +1,14 @@
 // singleton : When you make it from constructor only one object is there
+// Object.create // this known as constructor object through. In this only Singleton use
 
 //object literals
+const JsUser = {
+    name: "Shalini",
+    age: 25,
+    location: "Delhi",
+    email: "shalini@example.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+} // {} --> this is object 
+
+// How to access object 
