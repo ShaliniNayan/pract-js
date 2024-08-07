d546a45f73822cc1d7bb6e03cebda28e137794a8
Shalini Nayan <shalininayan@outlook.com>
2024-08-07 17:05:44 +0000

    Update Arrray.of

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -42,4 +42,4 @@ let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));// [ 100, 200, 300 ] --> .of : Return set of new element in single array
+console.log(Array.of(score1, score2, score3));// [ 100, 200, 300 ] --> .of : Return set of new array set of element
