23cd07b57419707c59491a17fecfe60eb149664e
Shalini Nayan <shalininayan@outlook.com>
2024-08-08 15:49:57 +0000

    Updated Symbol

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,8 +2,13 @@
 // Object.create // this known as constructor object through. In this only Singleton use
 
 //object literals
+
+const mySym = Symbol("key1") //Symbol
+
 const JsUser = {
     name: "Shalini",
+    "full name": "Shalini Nayan",
+    [mySym]: "mykey1", // if you have to write in symbol it should be written as []
     age: 25,
     location: "Delhi",
     email: "shalini@example.com",
@@ -14,3 +19,5 @@ const JsUser = {
 // How to access object 
 console.log(JsUser.email); // shalini@example.com --> many people do this but it's not correct way to access object
 console.log(JsUser["email"]); // shalini@example.com
+console.log(JsUser["full name"]); // Shalini Nayan
+console.log(JsUser[mySym]); // mykey1
