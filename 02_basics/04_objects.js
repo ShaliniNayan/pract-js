e2fba6d9637eb87fded0a131f47d03888e5c839b
Shalini Nayan <shalininayan@outlook.com>
2024-08-12 06:50:06 +0000

    Declare variable in singleton with help of construction

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,7 @@
+// Singleton with help of construction declare variable
+
+// const tinderUser = new Object() 
+// or
+const tinderUser = {}
+
+console.log(tinderUser); // {} --> in both way answer will be empty array
