b35d5380d3a221199d9d70e284869f7dce099a04
Shalini Nayan <shalininayan@outlook.com>
2024-08-18 16:00:25 +0000

    Update

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -35,4 +35,3 @@ const obj2 = {3: "a", 4: "b"}
 const obj3 = Object.assign({}, obj1, obj2) // .assign --> Copy the values of all of the enumerable own properties from one or more source objects to a target object. Returns the target object.
 // console.log(obj3); // { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
 console.log(obj3); // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
-// 
