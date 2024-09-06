738b006b91862148351e900b0c7ef3bbe9157583
Shalini Nayan <shalininayan@outlook.com>
2024-09-06 16:52:59 +0000

    Method

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -61,4 +61,8 @@ console.log(tinderUser); // { id: '123abc', name: 'Sammy', isLoggedIn: false }
 
 // Method
 console.log(Object.keys(tinderUser)); // [ 'id', 'name', 'isLoggedIn' ]
+console.log(Object.values(tinderUser)); // [ '123abc', 'Sammy', false ]
+console.log(Object.entries(tinderUser)); // [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); // true
+console.log(tinderUser.hasOwnProperty('isLogged')); // false
  
