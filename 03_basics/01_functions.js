a3037bceb269bc3d5911d368b6a2ae92144546da
Shalini Nayan <shalininayan@outlook.com>
2024-09-30 13:16:43 +0000

    Started function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,12 @@
+// Deffination 
+function sayMyName() {
+    console.log("S");
+    console.log("H");
+    console.log("L");
+    console.log("I");
+    console.log("N");
+    console.log("E");    
+}
+
+// reference and execute --> reference will be "sayMYName" and execute will be "()"
+sayMyName()
